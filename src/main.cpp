240ce98465e0fc322621073901cd6a23fd632faf
Dongjin Kim <djkim9031.research@gmail.com>
2024-06-13 02:34:53 +0000

    added supported operations - sub, div, pow, etc.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ using namespace micrograd;
 
 int main() {
     std::set<std::shared_ptr<Value>> init = {};
+   
     auto x1 = std::make_shared<Value>(2.0, init, "", "x1");
     auto x2 = std::make_shared<Value>(0.0, init, "", "x2");
     auto w1 = std::make_shared<Value>(-3.0, init, "", "w1");
@@ -18,10 +19,23 @@ int main() {
     auto bias = std::make_shared<Value>(6.8814, init, "", "b");
 
     auto n = (*temp) + (*bias);
-    auto o = n->tanh();
+    //auto o = n->tanh();
+    auto e = (2* (*n));
+    e = e->exp();
+    auto numer = (*e-1);
+    auto denom = (*e+1);
+    auto o = (*numer)/(*denom);
 
     o->backward();
+ 
+    /*
 
+    auto a = std::make_shared<Value>(-2.0, init, "", "a");
+    auto b = std::make_shared<Value>(3.0, init, "", "b");
+    auto d = (*a)/(*b);
+    auto e = d->pow_with_base(10);
+    e->backward();
+    */
 
     draw_plot(o);
 
